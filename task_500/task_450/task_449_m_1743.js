44637fe7e174b847260d2cb2d8e17a9c22e9f490
TristanSergey <tristan.sergey@gmail.com>
2023-11-11 14:08:33 +0300

    11112023

---
+++ task_500/task_450/task_449_m_1743.js
@@ -0,0 +1,51 @@
+﻿// Существует целочисленный массив nums, состоящий из n уникальных элементов, 
+// но вы его забыли.Однако вы помните каждую пару соседних элементов в числах.
+// Вам дан двумерный целочисленный массив смежных пар размера n - 1, 
+// где каждый смежный массив[i] = [ui, vi] указывает, что элементы ui и vi 
+// являются соседними в числах.
+// Гарантируется, что каждая соседняя пара элементов nums[i] и nums[i + 1] 
+// будет существовать в соседних парах либо как[nums[i], nums[i + 1]] 
+// либо[nums[i + 1], nums[я]].Пары могут появляться в любом порядке.
+// Верните исходные числа массива.Если существует несколько решений, 
+// верните любое из них.
+// 1743
+
+adjacentPairs = [[2, 1], [3, 4], [3, 2]]
+
+var restoreArray = function (adjacentPairs) {
+
+    const pairs = new Map();
+
+    for (const item of adjacentPairs) {
+        if (!pairs.has(item[0])) pairs.set(item[0], []);
+        if (!pairs.has(item[1])) pairs.set(item[1], []);
+        pairs.get(item[0]).push(item[1]);
+        pairs.get(item[1]).push(item[0]);
+    }
+
+    const answer = [];
+    let start = -Infinity;
+
+    for (const [entry, values] of pairs) {
+        if (values.length === 1) {
+            start = entry;
+            break;
+        }
+    }
+
+    let left = -Infinity;
+    answer.push(start);
+
+    for (let i = 1; i <= adjacentPairs.length; i++) {
+        const item = pairs.get(start);
+        const newval = item[0] !== left ? item[0] : item[1];
+        answer.push(newval);
+        left = start;
+        start = newval;
+    }
+
+    return answer;
+
+};
+
+console.log(restoreArray(adjacentPairs))
---
+++ task_500/task_450/task_450_h_2642.js
@@ -0,0 +1,52 @@
+﻿// Существует ориентированный взвешенный граф, состоящий из n узлов, 
+// пронумерованных от 0 до n - 1. Ребра графа изначально представлены 
+// заданными ребрами массива, где ребра[i] = [fromi, toi, EdgeCosti] 
+// означают, что существует край от fromi до toi со стоимостью EdgeCosti.
+// Реализуйте класс Graph:
+// Graph(int n, int[][] ребра) инициализирует объект с n узлами и заданными ребрами.
+// addEdge(int[] Edge) добавляет ребро в список ребер, 
+// где Edge = [from, to, EdgeCost].
+// Гарантируется, что перед добавлением этого узла между двумя узлами нет ребра.
+// int shortestPath(int node1, int node2) возвращает минимальную стоимость 
+// пути от node1 до node2.Если пути не существует, верните - 1. 
+// Стоимость пути — это сумма стоимостей ребер пути.
+// 2642
+
+var Graph = function (n, edges) {
+    this.n = n;
+    this.range = Array.from({ length: n }, () => Array(n).fill(Number.MAX_SAFE_INTEGER));
+
+    for (let i = 0; i < n; i++) this.range[i][i] = 0;
+
+    for (const edge of edges) this.range[edge[0]][edge[1]] = edge[2];
+
+
+    for (let k = 0; k < n; k++) {
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                this.range[i][j] = Math.min(
+                    this.range[i][j],
+                    this.range[i][k] + this.range[k][j]
+                );
+            }
+        }
+    }
+};
+
+Graph.prototype.addEdge = function (edge) {
+    if (this.range[edge[0]][edge[1]] <= edge[2]) return;
+    this.range[edge[0]][edge[1]] = edge[2];
+
+    for (let i = 0; i < this.n; i++) {
+        for (let j = 0; j < this.n; j++) {
+            this.range[i][j] = Math.min(
+                this.range[i][j],
+                this.range[i][edge[0]] + edge[2] + this.range[edge[1]][j]
+            );
+        }
+    }
+};
+Graph.prototype.shortestPath = function (node1, node2) {
+    if (this.range[node1][node2] === Number.MAX_SAFE_INTEGER) return -1;
+    return this.range[node1][node2];
+};
