10554de2025102d01fce83324be9d6196519d78e
TristanSergey <tristan.sergey@gmail.com>
2023-11-15 12:14:13 +0300

    15112023

---
+++ task_500/task_460/task_451_h_815.js
@@ -0,0 +1,73 @@
+﻿// Вам дан массив маршрутов, представляющий автобусные маршруты, 
+// где маршруты[i] — это автобусный маршрут, который i - й автобус повторяет вечно.
+// Например, если маршруты[0] = [1, 5, 7], это означает, что 0 - й автобус 
+// едет в последовательности 1 -> 5 -> 7 -> 1 -> 5 -> 7 -> 1 -> .. .навсегда.
+// Вы начнете с исходной остановки автобуса(изначально вы не находитесь ни в
+// одном автобусе) и хотите добраться до цели автобусной остановки.
+// Передвигаться между остановками можно только на автобусах.
+// Возвращает наименьшее количество автобусов, на которых вам нужно доехать 
+// от источника до цели.Верните - 1, если это невозможно.
+// 815
+
+
+routes = [[1, 2, 7], [3, 6, 7]], source = 1, target = 6
+
+
+var numBusesToDestination = function (routes, source, target) {
+
+    if (source === target) return 0;
+
+    const stopToRoutesMap = new Map(); // Мапа, где ключ - остановка, значение - маршруты, проходящие через эту остановку
+    const queue = [];
+    const visitedStops = new Set(); // Множество для отслеживания посещенных остановок
+    const visitedRoutes = new Set(); // Множество для отслеживания посещенных маршрутов
+
+    // Заполнение мапы stopToRoutesMap
+    for (let i = 0; i < routes.length; i++) {
+        for (const stop of routes[i]) {
+            if (!stopToRoutesMap.has(stop)) stopToRoutesMap.set(stop, []);
+            stopToRoutesMap.get(stop).push(i);
+        }
+    }
+
+    // Начальная итерация, добавляем все маршруты, проходящие через начальную остановку, в очередь
+    const initialRoutes = stopToRoutesMap.get(source);
+    for (const route of initialRoutes) {
+        queue.push(route);
+        visitedRoutes.add(route);
+    }
+
+    let steps = 1; // Начальный шаг
+
+    while (queue.length > 0) {
+        const currentQueueSize = queue.length;
+
+        for (let i = 0; i < currentQueueSize; i++) {
+            const currentRoute = queue.shift();
+
+            for (const stop of routes[currentRoute]) {
+                if (visitedStops.has(stop)) continue; // Пропускаем уже посещенные остановки
+                visitedStops.add(stop);
+
+                if (stop === target) return steps; // Если достигли целевой остановки, возвращаем количество шагов
+
+                const nextRoutes = stopToRoutesMap.get(stop);
+
+                for (const nextRoute of nextRoutes) {
+                    if (!visitedRoutes.has(nextRoute)) {
+                        queue.push(nextRoute);
+                        visitedRoutes.add(nextRoute);
+                    }
+                }
+            }
+        }
+
+        steps++; // Увеличиваем количество шагов после обработки текущего уровня маршрутов
+    }
+
+    return -1; // Если не удалось добраться до целевой остановки
+
+};
+
+
+console.log(numBusesToDestination(routes, source, target))
---
+++ task_500/task_460/task_452_m_2785.js
@@ -0,0 +1,57 @@
+﻿// Учитывая строку s с индексом 0, переставьте s, 
+// чтобы получить новую строку t такую, что:
+// Все согласные остаются на своих прежних местах.Более формально, 
+// если существует индекс i с 0 <= i < s.length такой, 
+// что s[i] является согласной, то t[i] = s[i].
+// Гласные должны быть отсортированы в порядке неубывания их значений ASCII.
+// Более формально, для пар индексов i, j с 0 <= i < j < s.length, таких, 
+// что s[i] и s[j] являются гласными, тогда t[i] не должно иметь более 
+// высокое значение ASCII, чем t[дж].
+// Верните полученную строку.
+// Гласные: «а», «е», «я», «о» и «у», и они могут записываться как в нижнем, 
+// так и в верхнем регистре.К согласным относятся все буквы, не являющиеся гласными.
+// 2785
+
+s = "lEetcOde";
+
+var sortVowels = function (s) {
+
+    // const vovels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'];
+    // const hash = s.split('').filter((item) => vovels.includes(item)).map((letter) => letter.charCodeAt(0));
+    // hash.sort((a, b) => a - b)
+    // let t = [];
+    // // console.log(hash)
+    // for (let i = s.length - 1; i >= 0; i--) {
+
+    //     if (vovels.includes(s[i])) {
+    //         t[i] = String.fromCharCode(hash.pop());
+    //     } else {
+    //         t[i] = s[i]
+    //     }
+    //     // console.log(t)
+    // }
+
+
+    // // console.log(hash)
+    // // console.log(t)
+    // return t.join('');
+
+
+    const vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'];
+    // const sortedVowels = vowels.sort();
+
+    let t = s.split('');
+    let vowelsInS = t.filter((char) => vowels.includes(char)).sort();
+
+    vowelsInS.forEach((vowel) => {
+        t[t.indexOf(vowel)] = vowelsInS.shift();
+    });
+
+    return t.join('');
+
+
+
+};
+
+
+console.log(sortVowels(s))
