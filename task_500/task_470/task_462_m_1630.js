42f59a5a778e3c7c294cd71caa05580b31726359
TristanSergey <tristan.sergey@gmail.com>
2023-11-24 12:36:23 +0300

    24112023

--- task_500/task_470/task_462_m_1630.js
+++ task_500/task_470/task_462_m_1630.js
@@ -8,8 +8,28 @@
 // 1630
 
 nums = [4, 6, 5, 9, 3, 7], l = [0, 0, 2], r = [2, 3, 5];
+nums = [-12, -9, -3, -12, -6, 15, 20, -25, -20, -15, -10], l = [0, 1, 6, 4, 8, 7], r = [4, 4, 9, 7, 9, 10]
 
 var checkArithmeticSubarrays = function (nums, l, r) {
+
+    const answer = [];
+
+    for (let i = 0; i < l.length; i++) {
+
+
+        const arr = nums.slice(l[i], r[i] + 1);
+        arr.sort((a, b) => a - b);
+
+        let progression = true;
+        for (let j = arr.length - 1; j > 1; j--) {
+            if ((arr[j] - arr[j - 1]) !== (arr[j - 1] - arr[j - 2])) {
+                progression = false;
+                break
+            };
+        }
+        (progression) ? answer.push(true) : answer.push(false);
+    }
+    return answer;
 
 };
 
