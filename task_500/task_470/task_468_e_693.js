6d1e4678f5bfd14946866aeaf006a6803400f2c3
TristanSergey <tristan.sergey@gmail.com>
2023-11-29 20:51:21 +0300

    29112023

---
+++ task_500/task_470/task_468_e_693.js
@@ -0,0 +1,16 @@
+﻿// Учитывая положительное целое число, проверьте, 
+// есть ли в нем чередующиеся биты: а именно, 
+// будут ли два соседних бита всегда иметь разные значения.
+// 693
+
+n = 85;
+
+var hasAlternatingBits = function (n) {
+    const bin = n.toString(2);
+    for (let i = 1; i < bin.length; i++) {
+        if (bin[i - 1] === bin[i]) return false;
+    }
+    return true;
+};
+
+console.log(hasAlternatingBits(n))
