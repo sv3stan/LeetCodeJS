c3d9e603bfc3c026fb42aa50341418e3dfae4f01
TristanSergey <tristan.sergey@gmail.com>
2023-12-01 12:39:45 +0300

    01122023

---
+++ task_500/task_470/task_470_e_1662.js
@@ -0,0 +1,36 @@
+﻿// Учитывая два массива строк word1 и word2, верните true,
+//  если два массива представляют одну и ту же строку, 
+//  и false в противном случае.
+// Строка представляется массивом, если элементы массива, 
+// объединенные по порядку, образуют строку.
+// 1662
+
+word1 = ["abc", "d", "defg"], word2 = ["abcddafg"]
+
+var arrayStringsAreEqual = function (word1, word2) {
+
+
+    console.log(word1.flatMap(str => str.split('')));
+
+    const arr1 = word1.flatMap(str => str.split(''));
+    const arr2 = word2.flatMap(str => str.split(''));
+    const len1 = arr1.length;
+    const len2 = arr2.length;
+    if (len1 !== len2) return false;
+    //let j = len1;
+    for (let i = 0, j = len1; i != j; i++, j--) {
+
+        if (arr1[i] !== arr2[i]) return false;
+        if (arr1[j] !== arr2[j]) return false;
+
+
+
+    }
+    console.log(arr2);
+
+    return true;
+
+};
+
+
+console.log(arrayStringsAreEqual(word1, word2))
