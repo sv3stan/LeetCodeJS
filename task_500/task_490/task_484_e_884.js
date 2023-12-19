75593449ccef20d3e73df5202fc32ed443dd8b32
TristanSergey <tristan.sergey@gmail.com>
2023-12-19 13:36:29 +0300

    19122023

---
+++ task_500/task_490/task_484_e_884.js
@@ -0,0 +1,46 @@
+﻿// Предложение представляет собой строку слов, разделенных одним пробелом, 
+// где каждое слово состоит только из строчных букв.
+// Слово считается необычным, если оно встречается ровно один раз в 
+// одном из предложений и не встречается в другом предложении.
+// Учитывая два предложения s1 и s2, верните список всех необычных слов.
+// Вы можете вернуть ответ в любом порядке.
+
+// 884
+
+s1 = "this apple is sweet", s2 = "this apple is sour";
+
+var uncommonFromSentences = function (s1, s2) {
+
+    const quantityFirst = {};
+    const quantitySecond = {};
+
+    const updateFrequency = (sentence, freq) => {
+        const words = sentence.split(' ');
+        for (const word of words) {
+            freq[word] = (freq[word] || 0) + 1;
+        }
+    };
+
+
+    updateFrequency(s1, quantityFirst);
+    updateFrequency(s2, quantitySecond);
+
+    const answer = [];
+
+    for (const word in quantityFirst) {
+        if (quantityFirst[word] === 1 && !quantitySecond[word]) {
+            answer.push(word);
+        }
+    }
+
+    for (const word in quantitySecond) {
+        if (quantitySecond[word] === 1 && !quantityFirst[word]) {
+            answer.push(word);
+        }
+    }
+
+    return answer;
+
+};
+
+console.log(uncommonFromSentences(s1, s2))
---
+++ task_500/task_490/task_485_m_2353.js
@@ -0,0 +1,95 @@
+﻿// Разработайте систему оценки продуктов питания,
+//     которая может выполнять следующие функции:
+// Измените рейтинг продукта, указанного в системе.
+// Возвращает продукт с самым высоким рейтингом для типа кухни в системе.
+
+// Реализуйте класс FoodRatings:
+// FoodRatings(String[] foods, String[] cuisines, int[] ratings) Инициализирует систему.
+// Продукты питания описываются foods, cuisines и ratings, каждая из которых имеет длину n.
+//         food[i] — название i - го блюда,
+//         cuisine[i] — это тип кухни i - го блюда, а
+//         ratings[i] — начальный рейтинг i - го блюда.
+
+//     voidchangeRating(String food, int newRating) Изменяет рейтинг продукта с названием food.
+
+//     String highestRated(String cuisine) Возвращает название продукта, 
+//     имеющего наивысший рейтинг для данного типа кухни.
+
+//     Если есть ничья, верните элемент с лексикографически меньшим именем.
+// Обратите внимание, что строка x лексикографически меньше строки y, 
+// если x стоит перед y в словарном порядке, то есть либо x является префиксом y, 
+// либо если i является первой позицией такой, 
+// что x[i] != y[i], то x[i] предшествует y[i] в ​​алфавитном порядке.
+
+// 2353
+
+
+/**
+ * @param {string[]} foods
+ * @param {string[]} cuisines
+ * @param {number[]} ratings
+ */
+
+const foodRatings = [[["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"], ["korean", "japanese", "japanese", "greek", "japanese", "korean"], [9, 12, 8, 15, 14, 7]], ["korean"], ["japanese"], ["sushi", 16], ["japanese"], ["ramen", 16], ["japanese"]]
+
+var FoodRatings = function (inputFoods, inputCuisines, inputRatings) {
+    const n = inputFoods.length
+
+    const foodHash = new Map()
+    const cuisineHash = new Map()
+    const foodRatingHash = new Map()
+
+    for (let i = 0; i < n; i++) {
+        const food = inputFoods[i]
+        const cuisine = inputCuisines[i]
+        const rating = inputRatings[i]
+        if (!foodHash.has(food)) foodHash.set(food, cuisine)
+        if (!cuisineHash.has(cuisine)) {
+            const foodRating = new PriorityQueue({ compare: (a, b) => b[1] - a[1] || a[0].localeCompare(b[0]) })
+            cuisineHash.set(cuisine, foodRating)
+        }
+        const foodRating = cuisineHash.get(cuisine)
+        foodRating.enqueue([food, rating])
+        foodRatingHash.set(food, rating)
+    }
+
+    /** 
+     * @param {string} food 
+     * @param {number} newRating
+     * @return {void}
+     */
+    FoodRatings.prototype.changeRating = function (targetFood, updatedRating) {
+        const cuisine = foodHash.get(targetFood)
+        const foodRating = cuisineHash.get(cuisine)
+        const [food, rating_unused] = foodRating.front()
+        if (food === targetFood) foodRating.dequeue()
+        foodRating.enqueue([targetFood, updatedRating])
+        foodRatingHash.set(targetFood, updatedRating)
+    };
+
+    /** 
+     * @param {string} cuisine
+     * @return {string}
+     */
+    FoodRatings.prototype.highestRated = function (targetCuisine) {
+        const foodRating = cuisineHash.get(targetCuisine)
+        while (1) {
+            const [food, rating] = foodRating.front()
+            const newRating = foodRatingHash.get(food)
+            if (rating === newRating) return food
+            else {
+                foodRating.dequeue()
+                foodRating.enqueue([food, newRating])
+            }
+        }
+    }
+};
+
+
+
+var obj = new FoodRatings(foods, cuisines, ratings)
+obj.changeRating(food, newRating)
+var param_2 = obj.highestRated(cuisine)
+
+
+console.log(foodRatings.highestRated("korean"));
