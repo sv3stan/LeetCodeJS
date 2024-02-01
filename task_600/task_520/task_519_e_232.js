8d83754bb91c13f58280990a6394e63cc107fb76
TristanSergey <tristan.sergey@gmail.com>
2024-02-01 13:59:30 +0300

    01022024

---
+++ task_600/task_520/task_519_e_232.js
@@ -0,0 +1,56 @@
+﻿// Реализуйте очередь «первым пришел — первым обслужен» (FIFO),
+// используя только два стека.Реализованная очередь должна
+// поддерживать все функции обычной очереди(отправка, просмотр, извлечение и очистка).
+// Реализуйте класс MyQueue:
+// void push(int x) Помещает элемент x в конец очереди.
+// int pop() Удаляет элемент из начала очереди и возвращает его.
+// int peek() Возвращает элемент в начале очереди.
+// boolean пустой() Возвращает true, если очередь пуста, в противном случае — false.
+
+// 232
+
+var MyQueue = function () {
+    this.queue = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+    this.queue.unshift(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+    return this.queue.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+    return this.queue[this.queue.length - 1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+    return this.queue.length === 0
+};
+
+
+let myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+console.log(myQueue)
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+console.log(myQueue)
+myQueue.peek(); // return 1
+console.log(myQueue)
+myQueue.pop(); // return 1, queue is [2]
+console.log(myQueue)
+myQueue.empty(); // return false
+console.log(myQueue)
---
+++ task_600/task_520/task_520_e_496.js
@@ -0,0 +1,51 @@
+﻿// Следующий больший элемент некоторого элемента x в массиве — это первый больший элемент, 
+// находящийся справа от x в том же массиве.
+// Вам даны два различных целочисленных массива с нулевым индексом nums1 и nums2, 
+// где nums1 — это подмножество nums2.
+// Для каждого 0 <= i < nums1.length найдите индекс j такой, 
+// что nums1[i] == nums2[j] и определите следующий больший элемент nums2[j] в nums2.
+// Если следующего большего элемента нет, то ответом на этот запрос будет - 1.
+// Верните массив ans длины nums1.length такой, что ans[i] является следующим большим элементом, 
+// как описано выше.
+
+// 496
+
+nums1 = [4, 1, 2], nums2 = [1, 3, 4, 2]
+//nums1 = [2, 4], nums2 = [1, 2, 3, 4]
+
+var nextGreaterElement = function (nums1, nums2) {
+
+    const result = Array.from({ length: nums1.length }).fill(-1);
+    const stack = [];
+
+    for (let j = 0; j < nums1.length; j++) {
+
+        //const index = 
+
+        for (let i = nums2.indexOf(nums1[j]); i < nums2.length; i++) {
+
+            while (stack.length > 0 && nums2[i] <= stack[stack.length - 1]) {
+                stack.pop();
+            }
+
+            if (nums2[i] > nums1[j]) {
+                result[j] = nums2[i];
+                break;
+            }
+
+            stack.push(nums2[i])
+        }
+
+    }
+
+
+
+
+    return result;
+
+};
+
+
+console.log(nextGreaterElement(nums1, nums2))
+
+
