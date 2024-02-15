5495941f13dcbc41b8b35e2e7a93a47c76d13560
TristanSergey <tristan.sergey@gmail.com>
2024-02-15 16:05:54 +0300

    15022024

---
+++ task_600/task_540/task_531_e_2404.js
@@ -0,0 +1,36 @@
+﻿// Учитывая целочисленный массив nums, верните наиболее часто встречающийся четный элемент.
+
+// Если есть ничья, верните наименьшую.Если такого элемента нет, верните - 1.
+
+// 2404
+
+
+nums = [4, 0, 1, 2, 2, 2, 2, 4, 4, 1]
+nums = [29, 47, 21, 41, 13, 37, 25, 7]
+nums = [4, 4, 4, 9, 2, 4]
+nums = [0, 1, 4, 4, 2, 2, 1]
+
+var mostFrequentEven = function (nums) {
+
+    const hash = new Map();
+    let maxFreq = 0;
+    let max = -1;
+
+    const arr = nums.filter((item) => (item % 2 === 0)).sort((a, b) => a - b);
+
+    for (let i = 0; i < arr.length; i++) {
+        hash.set(arr[i], (hash.get(arr[i]) || 0) + 1);
+    }
+
+
+    for (let [key, value] of hash.entries()) {
+        if (value > maxFreq) {
+            max = key;
+            maxFreq = value;
+        }
+    }
+    return max;
+};
+
+
+console.log(mostFrequentEven(nums))
---
+++ task_600/task_540/task_532_e_2108.js
@@ -0,0 +1,41 @@
+﻿// Учитывая массив строковых слов, верните первую палиндромную строку в массиве.
+// Если такой строки нет, верните пустую строку «».
+// Строка является палиндромной, если она читается одинаково и вперед, и назад.
+
+// 2108
+
+
+words = ["abc", "car", "adceda", "racecar", "cool"]
+
+var firstPalindrome = function (words) {
+
+
+    function isPalindrome(str) {
+        console.log('++++++++++')
+        let i = 0;
+        let j = str.length - 1;
+        while (i < j) {
+            console.log(str[i] + ' -  str[i] - ' + str[j] + ' - str[j]')
+            if (str[i] !== str[j]) {
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+    }
+
+
+    for (let i = 0; i < words.length; i++) {
+
+        if (isPalindrome(words[i])) {
+            return words[i]
+        }
+
+    }
+
+    return "";
+};
+
+console.log(firstPalindrome(words))
