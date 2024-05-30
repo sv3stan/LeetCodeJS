c959b9dad5a767bb8002a39878ebdec2491d5b1d
TristanSergey <tristan.sergey@gmail.com>
2024-05-30 20:27:43 +0300

    30052024

---
+++ task_700/task_640/task_637_m_1208.js
@@ -0,0 +1,41 @@
+﻿/*
+Вам даны две строки s и t одинаковой длины и целое число maxCost.
+Вы хотите изменить s на t. Замена i-го символа s на i-й символ t стоит |s[i] - t[i]| (т. е. абсолютная разница между значениями ASCII символов).
+Возвращает максимальную длину подстроки s, которую можно изменить так, чтобы она совпадала с соответствующей подстрокой t со стоимостью, меньшей или равной maxCost. Если нет подстроки из s, которую можно было бы заменить на соответствующую подстроку из t, верните 0.
+*/
+
+let s = 'abcd';
+let t = 'bcdf';
+let maxCost = 3;
+
+// (s = 'abcd'), (t = 'cdef'), (maxCost = 3);
+
+let equalSubstring = function (s, t, maxCost) {
+    const sCharCodes = s.split('').map(char => char.charCodeAt(0));
+    const tCharCodes = t.split('').map(char => char.charCodeAt(0));
+
+    const difference = sCharCodes.map((code, index) =>
+        Math.abs(code - tCharCodes[index]),
+    );
+
+    let left = 0;
+    let right = 0;
+    let sum = 0;
+    let maxLength = 0;
+
+    while (right < difference.length) {
+        sum += difference[right];
+
+        while (sum > maxCost) {
+            sum -= difference[left];
+            left++;
+        }
+
+        maxLength = Math.max(maxLength, right - left + 1);
+        right++;
+    }
+
+    return maxLength;
+};
+
+console.log(equalSubstring(s, t, maxCost));
---
+++ task_700/task_640/task_639_m_1442.js
@@ -0,0 +1,35 @@
+﻿/*
+Дан массив целых чисел arr.
+Мы хотим выбрать три индекса i, j и k, где (0 <= i < j <= k < arr.length).
+Определим a и b следующим образом:
+a = arr[i] ^ arr[i + 1] ^ ... ^ arr[j - 1]
+b = arr[j] ^ arr[j + 1] ^ ... ^ arr[k]
+Обратите внимание, что ^ обозначает операцию побитового исключения.
+Возвращает количество троек (i, j и k), где a == b.
+*/
+
+let arr = [2, 3, 1, 6, 7];
+
+let countTriplets = function (arr) {
+    const prefixXOR = [...arr];
+    prefixXOR.unshift(0);
+    const length = prefixXOR.length;
+
+    for (let i = 1; i < length; ++i) {
+        prefixXOR[i] ^= prefixXOR[i - 1];
+    }
+
+    let count = 0;
+
+    for (let begin = 0; begin < length; ++begin) {
+        for (let end = begin + 1; end <= length; ++end) {
+            if (prefixXOR[begin] == prefixXOR[end]) {
+                count += end - begin - 1;
+            }
+        }
+    }
+
+    return count;
+};
+
+console.log(countTriplets(arr));
