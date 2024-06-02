e045e2199090bc69bb73d258f7f892c1fe1abbd5
TristanSergey <tristan.sergey@gmail.com>
2024-06-02 18:03:51 +0300

    02062024

---
+++ task_700/task_650/task_641_e_3110.js
@@ -0,0 +1,17 @@
+﻿/*
+Вам дана строка s. Оценка строки определяется как сумма абсолютной разницы между значениями ASCII соседних символов.
+Вернуть оценку s.
+*/
+let s = 'hello';
+
+let scoreOfString = function (s) {
+    let res = 0;
+
+    for (let i = 1; i < s.length; i++) {
+        res += Math.abs(s[i].charCodeAt() - s[i - 1].charCodeAt());
+    }
+
+    return res;
+};
+
+console.log(scoreOfString(s));
