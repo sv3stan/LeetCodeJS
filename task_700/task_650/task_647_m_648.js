71b142ab9efe0fc1d7fcff4bc8d85f6ac47e74e9
TristanSergey <tristan.sergey@gmail.com>
2024-06-09 00:05:16 +0300

    08062024

---
+++ task_700/task_650/task_647_m_648.js
@@ -0,0 +1,33 @@
+﻿/**
+В английском языке есть понятие, называемое корнем, за которым может следовать какое-либо другое слово, образуя еще одно более длинное слово - давайте назовем это слово производным. Например, когда за корнем "help" следует слово "full", мы можем образовать производное "helpful".
+При наличии словаря, состоящего из множества корней, и предложения, состоящего из слов, разделенных пробелами, замените все производные в предложении на корень, который их образует. Если производное может быть заменено более чем одним корнем, замените его корнем, который имеет наименьшую длину.
+Верните предложение после замены.
+ */
+
+let dictionary = ['cat', 'bat', 'rat'];
+let sentence = 'the cattle was rattled by the battery';
+
+let replaceWords = function (dictionary, sentence) {
+    const hash = new Set(dictionary);
+    const words = sentence.split(' ');
+    const result = [];
+
+    for (const word of words) {
+        let isFound = false;
+        for (let i = 1; i <= word.length; i++) {
+            const prefix = word.slice(0, i);
+            if (hash.has(prefix)) {
+                result.push(prefix);
+                isFound = true;
+                break;
+            }
+        }
+        if (!isFound) {
+            result.push(word);
+        }
+    }
+
+    return result.join(' ');
+};
+
+console.log(replaceWords(dictionary, sentence));
---
+++ task_700/task_650/task_648_e_10004.js
@@ -0,0 +1,25 @@
+﻿/*
+Реализуйте базовую функцию querySelectorAll, которая принимает на вход строку селектора и возвращает массив элементов DOM, соответствующих данному селектору.
+*/
+export const querySelectorAll = selector => {
+    const result = [];
+
+    if (selector.startsWith('.')) {
+        const classItem = document.getElementsByClassName(selector.slice(1));
+        for (let i = 0; i < classItem.length; i++) {
+            result.push(classItem[i]);
+        }
+    } else if (/^[a-zA-Z]+$/.test(selector)) {
+        const tagItem = document.getElementsByTagName(selector);
+        for (let i = 0; i < tagItem.length; i++) {
+            result.push(tagItem[i]);
+        }
+    } else if (selector.startsWith('#')) {
+        const idItem = document.getElementById(selector.slice(1));
+        if (idItem) {
+            result.push(idItem);
+        }
+    }
+
+    return result;
+};
