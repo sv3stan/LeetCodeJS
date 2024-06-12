d939961ab28ba85b8c043e8868ccbb754e19cf05
TristanSergey <tristan.sergey@gmail.com>
2024-06-12 15:45:33 +0300

    12062024

---
+++ task_700/task_650/task_649_e_83.js
@@ -0,0 +1,21 @@
+﻿/*
+Учитывая заголовок отсортированного связанного списка, удалите все дубликаты, чтобы каждый элемент появлялся только один раз. Верните связанный список также отсортированным.
+*/
+let deleteDuplicates = function (head) {
+    const hash = new Set();
+    let current = head;
+    let prev = null;
+    while (current !== null) {
+        if (hash.has(current.val)) {
+            prev.next = current.next;
+        } else {
+            hash.add(current.val);
+            prev = current;
+        }
+        current = current.next;
+    }
+
+    return head;
+};
+
+console.log(deleteDuplicates(head));
---
+++ task_700/task_660/task_651_e_1122.js
@@ -0,0 +1,26 @@
+﻿/**
+ Учитывая два массива arr1 и arr2, элементы arr2 различны, и все элементы в arr2 также находятся в arr1.
+Отсортируйте элементы arr1 так, чтобы относительный порядок элементов в arr1 был таким же, как и в arr2. Элементы, которых нет в arr2, следует размещать в конце arr1 в порядке возрастания.
+ */
+
+let arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19];
+let arr2 = [2, 1, 4, 3, 9, 6];
+
+let relativeSortArray = function (arr1, arr2) {
+    const hash = new Set(arr2);
+    const result = [];
+
+    for (let item of hash) {
+        let index = arr1.indexOf(item);
+        while (index !== -1) {
+            result.push(item);
+            arr1.splice(index, 1);
+            index = arr1.indexOf(item);
+        }
+    }
+
+    result.push(...arr1.sort((a, b) => a - b));
+    return result;
+};
+
+console.log(relativeSortArray(arr1, arr2));
