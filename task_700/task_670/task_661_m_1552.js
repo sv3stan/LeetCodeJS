b188e884fe1224b0b54a4effe8b087fe87a29deb
TristanSergey <tristan.sergey@gmail.com>
2024-06-22 14:22:24 +0300

    22062024

---
+++ task_700/task_670/task_661_m_1552.js
@@ -0,0 +1,46 @@
+﻿/**
+Во вселенной Земля C-137 Рик обнаружил особую форму магнитной силы между двумя шариками, если их положить в его новую изобретенную корзину. У Рика n пустых корзин, i-я корзина находится в позиции [i], у Морти есть m шаров, и ему нужно распределить шары по корзинам так, чтобы минимальная магнитная сила между любыми двумя шарами была максимальной.
+Рик заявил, что магнитная сила между двумя разными шарами в положениях x и y равна |x - y|.
+Учитывая позицию целочисленного массива и целое число m. Верните необходимую силу
+ */
+
+let position = [1, 2, 3, 4, 7];
+let m = 3;
+
+let maxDistance = function (position, m) {
+    position.sort((a, b) => a - b);
+
+    function helper(minForce) {
+        let count = 1;
+        let lastPosition = position[0];
+
+        for (let i = 1; i < position.length; i++) {
+            if (position[i] - lastPosition >= minForce) {
+                count++;
+                lastPosition = position[i];
+                if (count >= m) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    let left = 1;
+    let right = position[position.length - 1] - position[0];
+    let ans = 0;
+
+    while (left <= right) {
+        let middle = (left + right) >> 1;
+        if (helper(middle)) {
+            ans = middle;
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
+    }
+
+    return ans;
+};
+
+console.log(maxDistance(position, m));
---
+++ task_700/task_670/task_663_m_1248.js
@@ -0,0 +1,34 @@
+﻿/**
+Дан массив целых чисел nums и целое число k. Непрерывный подмассив называется хорошим, если в нем содержится k нечетных чисел.
+Возвращает количество хороших подмассивов.
+*/
+
+let nums = [1, 1, 2, 1, 1];
+let k = 3;
+nums = [2, 2, 2, 1, 2, 2, 1, 2, 2, 2];
+k = 2;
+
+let numberOfSubarrays = function (nums, k) {
+    let cnt = 0;
+    let oddQuantity = 0;
+    const freq = { 0: 1 }; // начальный элемент для учета префиксных сумм
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 !== 0) {
+            oddQuantity++;
+        }
+
+        if (freq[oddQuantity - k] !== undefined) {
+            cnt += freq[oddQuantity - k];
+        }
+
+        if (freq[oddQuantity] === undefined) {
+            freq[oddQuantity] = 0;
+        }
+        freq[oddQuantity]++;
+    }
+
+    return cnt;
+};
+
+console.log(numberOfSubarrays(nums, k));
