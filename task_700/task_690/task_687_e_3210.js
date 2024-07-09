25664f13c464fe1c5b756d710620ab6042aa961f
TristanSergey <tristan.sergey@gmail.com>
2024-07-09 22:03:13 +0300

    09072024

---
+++ task_700/task_690/task_687_e_3210.js
@@ -0,0 +1,19 @@
+﻿/**
+Вам дана строка s и целое число k. Зашифруйте строку, используя следующий алгоритм:
+Для каждого символа c в s замените c на k-й символ после c в строке (циклически).
+Верните зашифрованную строку.
+*/
+
+let s = 'abcdefghjk';
+let k = 4;
+
+let getEncryptedString = function (s, k) {
+    let answer = '';
+    for (let i = 0; i < s.length; i++) {
+        answer += s[(i + k) % s.length];
+    }
+
+    return answer;
+};
+
+console.log(getEncryptedString(s, k));
---
+++ task_700/task_690/task_689_m_1701.js
@@ -0,0 +1,36 @@
+﻿/**
+Есть ресторан с одним шеф-поваром. Вам дан массив клиентов, где клиенты[i] = [arrivali, timei]:
+прибытиеi — время прибытия i-го клиента. Время прибытия отсортировано в неубывающем порядке.
+timei — время, необходимое для подготовки заказа i-го покупателя.
+Когда приходит клиент, он передает шеф-повару свой заказ, а шеф-повар приступает к его приготовлению, когда он простаивает. Клиент ждет, пока шеф-повар закончит готовить заказ. Шеф-повар не готовит еду более чем для одного гостя одновременно. Шеф-повар готовит еду для клиентов в том порядке, в котором они были указаны во входных данных.
+Возвращает среднее время ожидания всех клиентов. Решения в пределах 10-5 от фактического ответа считаются принятыми.
+ */
+
+let customers = [
+    [5, 2],
+    [5, 4],
+    [10, 3],
+    [20, 1],
+];
+
+let averageWaitingTime = function (customers) {
+    const waitingTime = Array.from({ length: customers.length }).fill(0);
+
+    let newTime = 0;
+    // customers[0][0] + customers[0][1];
+    // waitingTime[0] = newTime - customers[0][0];
+
+    for (let i = 0; i < customers.length; i++) {
+        if (newTime > customers[i][0]) {
+            waitingTime[i] = newTime - customers[i][0] + customers[i][1];
+            newTime = newTime + customers[i][1];
+        } else {
+            waitingTime[i] = customers[i][1];
+            newTime = customers[i][0] + customers[i][1];
+        }
+    }
+
+    return waitingTime.reduce((acc, val) => acc + val, 0) / waitingTime.length;
+};
+
+console.log(averageWaitingTime(customers));
