b0975977c61b09ee453a74bab81fd5c0d167cc41
TristanSergey <tristan.sergey@gmail.com>
2024-07-28 18:09:46 +0300

    28072024

---
+++ task_800/task_710/task_708_m_2976.js
@@ -0,0 +1,60 @@
+﻿/**
+Вам даны две исходные и целевые строки с нулевым индексом, обе длиной n и состоящие из строчных английских букв. Вам также даны два исходных и измененных символьных массива с индексом 0, а также стоимость целочисленного массива, где стоимость[i] представляет собой стоимость замены исходного символа[i] на измененный[i].
+Вы начинаете с источника строки. За одну операцию вы можете выбрать символ x из строки и заменить его на символ y стоимостью z, если существует какой-либо индекс j такой, что cost[j] == z, original[j] == x и изменено[j] == y.
+Возвращает минимальную стоимость преобразования источника строки в цель строки с помощью любого количества операций. Если невозможно преобразовать источник в цель, верните -1.
+Обратите внимание, что могут существовать индексы i, j такие, что original[j] == original[i] и измененный[j] == измененный[i].
+
+Постройте граф, в котором каждая буква является узлом, и постройте edge(a, b) с весом c, если мы можем перейти от символа a к букве b со стоимостью c. (Оставьте тот, у которого наименьшая стоимость, на случай, если между a и b имеется несколько ребер).
+
+Вычислите кратчайший путь для каждой пары символов (source[i], target[i]). Сумма стоимости по всем i в диапазоне [0, source.length - 1]. Если между source[i] и target[i] нет пути, ответ — -1.
+
+Любые алгоритмы кратчайшего пути будут работать, поскольку у нас всего 26 узлов. Поскольку у нас есть не более 26 * 26 пар, мы можем сохранить результат, чтобы избежать повторного расчета.
+
+Мы также можем использовать алгоритм Флойда Уоршалла для предварительного вычисления всех результатов.
+ */
+
+let source = 'abcd';
+let target = 'acbe';
+let original = ['a', 'b', 'c', 'c', 'e', 'd'];
+let changed = ['b', 'c', 'b', 'e', 'b', 'e'];
+let cost = [2, 5, 5, 1, 2, 20];
+
+let minimumCost = function (source, target, original, changed, cost) {
+    const ALPHABET_LENGTH = 26;
+
+    let ans = 0;
+    const graph = Array.from({ length: ALPHABET_LENGTH }, () =>
+        Array(ALPHABET_LENGTH).fill(Infinity),
+    );
+
+    for (let i = 0; i < original.length; ++i) {
+        let from = original[i].charCodeAt(0) - 'a'.charCodeAt(0);
+        let to = changed[i].charCodeAt(0) - 'a'.charCodeAt(0);
+        graph[from][to] = Math.min(graph[from][to], cost[i]);
+    }
+
+    for (let k = 0; k < ALPHABET_LENGTH; k++) {
+        for (let i = 0; i < ALPHABET_LENGTH; i++) {
+            for (let j = 0; j < ALPHABET_LENGTH; j++) {
+                graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+            }
+        }
+    }
+
+    for (let i = 0; i < source.length; i++) {
+        if (source[i] === target[i]) continue;
+
+        const from = source[i].charCodeAt(0) - 'a'.charCodeAt(0);
+        const to = target[i].charCodeAt(0) - 'a'.charCodeAt(0);
+
+        if (graph[from][to] === Infinity) {
+            return -1;
+        }
+
+        ans += graph[from][to];
+    }
+
+    return ans;
+};
+
+console.log(minimumCost(source, target, original, changed, cost));
---
+++ task_800/task_710/task_709_h_2045.js
@@ -0,0 +1,70 @@
+﻿/**
+Город представляется в виде двунаправленного связного графа с n вершинами, где каждая вершина помечена от 1 до n (включительно). Ребра в графе представлены как ребра двумерного целочисленного массива, где каждое edges [i] = [ui, vi] обозначает двунаправленное ребро между вершиной ui и вершиной vi. Каждая пара вершин соединена не более чем одним ребром, и ни одна вершина не имеет ребра сама по себе. Время, необходимое для прохождения любого ребра, составляет time минут.
+В каждой вершине есть светофор, который каждые change минут меняет свой цвет с зеленого на красный и наоборот. Все сигналы изменяются одновременно. Вы можете войти в вершину в любое время, но покинуть вершину можно только тогда, когда сигнал зеленый. Вы не можете ждать в вершине, если сигнал зеленый.
+Второе минимальное значение определяется как наименьшее значение, строго превышающее минимальное значение.
+Например, второе минимальное значение [2, 3, 4] равно 3, а второе минимальное значение [2, 2, 4] равно 4.
+Учитывая n, ребра, время и изменение, верните второе минимальное время, необходимое для перехода от вершины 1 к вершине n.
+Примечания:
+Через любую вершину можно пройти любое количество раз, включая 1 и n.
+Можно предположить, что когда путешествие начнется, все сигналы только что загорелись зеленым.
+ */
+
+let n = 5;
+let edges = [
+    [1, 2],
+    [1, 3],
+    [1, 4],
+    [3, 4],
+    [4, 5],
+];
+let time = 3;
+let change = 5;
+let secondMinimum = function (n, edges, time, change) {
+    const graph = Array.from({ length: n + 1 }, () => []);
+    const visitTimes = Array.from({ length: n + 1 }, () => []);
+    for (const [u, v] of edges) {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+
+    const queue = [1];
+    let currentTime = 0;
+    let result = -1;
+
+    while (queue.length > 0) {
+        const levelSize = queue.length;
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift();
+
+            if (node === n) {
+                if (result !== -1) {
+                    return currentTime;
+                }
+                result = currentTime;
+            }
+
+            for (const neighbour of graph[node]) {
+                const neighbourTimes = visitTimes[neighbour];
+
+                if (
+                    neighbourTimes.length === 0 ||
+                    (neighbourTimes.length === 1 &&
+                        neighbourTimes[0] !== currentTime)
+                ) {
+                    queue.push(neighbour);
+                    visitTimes[neighbour].push(currentTime);
+                }
+            }
+        }
+
+        if (Math.floor(currentTime / change) % 2 === 1) {
+            currentTime += change - (currentTime % change);
+        }
+
+        currentTime += time;
+    }
+
+    return -1;
+};
+
+console.log(secondMinimum(n, edges, time, change));
