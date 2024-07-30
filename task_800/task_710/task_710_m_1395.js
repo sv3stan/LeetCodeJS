1fc3f1a05fd1737d0124624b5c9d5f874fb4aa38
TristanSergey <tristan.sergey@gmail.com>
2024-07-30 21:59:53 +0300

    30072024

---
+++ task_800/task_710/task_710_m_1395.js
@@ -0,0 +1,41 @@
+﻿/**
+В шеренге стоят n солдат. Каждому солдату присвоено уникальное значение рейтинга.
+Вам необходимо сформировать из них команду из 3 солдат по следующим правилам:
+Выберите 3 солдата с индексом (i, j, k) и рейтингом (рейтинг[i], рейтинг[j], рейтинг[k]).
+Команда засчитывается, если: (рейтинг[i] < рейтинг[j] < рейтинг[k]) или (рейтинг[i] > рейтинг[j] > рейтинг[k]), где (0 <= i < j < k < н).
+Возвращает количество команд, которые вы можете сформировать с учетом условий. (солдаты могут входить в несколько команд).
+ */
+
+let rating = [2, 5, 3, 4, 1];
+
+let numTeams = function (rating) {
+    const n = rating.length;
+    const inc = Array.from({ length: n }, () => Array(4).fill(0));
+    const dec = Array.from({ length: n }, () => Array(4).fill(0));
+    for (let i = 0; i < n; i++) {
+        inc[i][1] = 1;
+        dec[i][1] = 1;
+    }
+
+    for (let count = 2; count <= 3; count++) {
+        for (let i = 0; i < n; i++) {
+            for (let j = i + 1; j < n; j++) {
+                if (rating[j] > rating[i]) {
+                    inc[j][count] += inc[i][count - 1];
+                }
+                if (rating[j] < rating[i]) {
+                    dec[j][count] += dec[i][count - 1];
+                }
+            }
+        }
+    }
+
+    let res = 0;
+    for (let i = 0; i < n; i++) {
+        res += inc[i][3] + dec[i][3];
+    }
+
+    return res;
+};
+
+console.log(numTeams(rating));
---
+++ task_800/task_720/task_711_m_1653.js
@@ -0,0 +1,27 @@
+﻿/*
+Вам дана строка s, состоящая только из символов «a» и «b».
+Вы можете удалить любое количество символов в s, чтобы сделать s сбалансированным. s сбалансирован, если не существует пары индексов (i,j) такой, что i < j и s[i] = 'b' и s[j]= 'a'.
+Возвращает минимальное количество удалений, необходимое для балансировки s.
+ */
+
+s = 'aababbab';
+
+let minimumDeletions = function (s) {
+    const n = s.length;
+    const dp = Array.from({ length: n + 1 }, () => 0);
+
+    let b_cnt = 0;
+
+    for (let i = 0; i < n; i++) {
+        if (s[i] == 'b') {
+            dp[i + 1] = dp[i];
+            b_cnt++;
+        } else {
+            dp[i + 1] = Math.min(dp[i] + 1, b_cnt);
+        }
+    }
+
+    return dp[n];
+};
+
+console.log(minimumDeletions(s)); // Output: 2
