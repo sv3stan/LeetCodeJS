e0eb81e52320310828f55f4faefc147057e8b766
TristanSergey <tristan.sergey@gmail.com>
2024-07-31 23:17:56 +0300

    31072024

---
+++ task_800/task_720/task_714_e_2696.js
@@ -0,0 +1,47 @@
+﻿/**
+Вам дана строка s, состоящая только из заглавных английских букв.
+Вы можете применить к этой строке некоторые операции, в одной из которых вы можете удалить из s любое вхождение одной из подстрок "AB" или "CD".
+Верните минимально возможную длину результирующей строки, которую вы можете получить.
+Обратите внимание, что после удаления подстроки строка конкатенируется и могут появиться новые подстроки "AB" или "CD".
+ */
+
+s = 'ABFCACDB';
+
+let minLength = function (s) {
+    const chars = s.split('');
+    let i = 0;
+    while (i < chars.length) {
+        if (chars[i] === 'A' && chars[i + 1] === 'B') {
+            chars.splice(i, 2);
+            i = 0;
+        } else if (chars[i] === 'C' && chars[i + 1] === 'D') {
+            chars.splice(i, 2);
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+    return chars.length;
+};
+
+console.log(minLength(s)); // Output: 6
+
+// Test case 2
+
+s = 'CDCDCD';
+
+console.log(minLength(s)); // Output: 0
+
+// Test case 3
+
+s = 'ABABAB';
+
+console.log(minLength(s)); // Output: 0
+
+// Test case 4
+
+s = 'ABCDCDCD';
+
+console.log(minLength(s)); // Output: 1
+
+// Test case 5
