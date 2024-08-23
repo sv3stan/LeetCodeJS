69a6dbc0626a9277ad9d6639195eec40f836d6af
s3stan <tristan.sergey@gmail.com>
2024-08-23 23:11:23 +0300

    230820224

---
+++ task_800/task_740/task_739_e_476.js
@@ -0,0 +1,19 @@
+﻿/**
+Дополнение целого числа — это целое число, которое вы получаете, если перевернуть все 0 на 1 и все 1 на 0 в его двоичном представлении.
+Например, целое число 5 — это «101» в двоичном формате, а его дополнение — «010», которое представляет собой целое число 2.
+Учитывая целое число, верните его дополнение.
+ */
+let num = 5;
+
+let findComplement = function (num) {
+    const bin = num.toString(2);
+    let inversionBin = '';
+
+    for (let i = 0; i < bin.length; i++) {
+        inversionBin += bin[i] === '0' ? '1' : '0';
+    }
+
+    return parseInt(inversionBin, 2);
+};
+
+console.log(findComplement(num));
---
+++ task_800/task_740/task_740_m_592.js
@@ -0,0 +1,59 @@
+﻿/**
+Учитывая строковое выражение, представляющее выражение сложения и вычитания дробей, верните результат вычисления в строковом формате.
+Конечным результатом должна быть несократимая дробь. Если конечный результат — целое число, измените его на формат дроби со знаменателем 1. Таким образом, в этом случае 2 следует преобразовать в 2/1.
+ */
+
+let expression = '-1/2+1/2+1/3';
+
+let fractionAddition = function (expression) {
+    let numerator = 0;
+    let denominator = 1;
+
+    function GCD(a, b) {
+        while (b) {
+            [a, b] = [b, a % b];
+        }
+        return a;
+    }
+
+    let i = 0;
+    while (i < expression.length) {
+        let currNum = 0;
+        let currDenominator = 0;
+
+        let negative = false;
+
+        if (expression[i] === '-' || expression[i] === '+') {
+            if (expression[i] === '-') {
+                negative = true;
+            }
+            i++;
+        }
+
+        while (i < expression.length && /\d/.test(expression[i])) {
+            currNum = currNum * 10 + (expression[i] - '0');
+            i++;
+        }
+
+        if (negative) currNum *= -1;
+
+        i++;
+
+        while (i < expression.length && /\d/.test(expression[i])) {
+            currDenominator = currDenominator * 10 + (expression[i] - '0');
+            i++;
+        }
+
+        numerator = numerator * currDenominator + currNum * denominator;
+        denominator = denominator * currDenominator;
+    }
+
+    let gcd = GCD(Math.abs(numerator), denominator);
+
+    numerator /= gcd;
+    denominator /= gcd;
+
+    return `${numerator}/${denominator}`;
+};
+
+console.log(fractionAddition(expression));
