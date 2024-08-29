db1339c66e4f827d8e0a7a9a3b9197f01b4390d6
s3stan <tristan.sergey@gmail.com>
2024-08-29 20:53:23 +0300

    29082024

---
+++ task_800/task_750/task_744_m_1514.js
@@ -0,0 +1,40 @@
+﻿/*
+Вам дан неориентированный взвешенный граф из n узлов (с индексом 0), представленный списком ребер, где ребра [i] = [a, b] — это неориентированное ребро, соединяющее узлы a и b с вероятностью успешного прохождения, которая край succProb[i].
+Учитывая начало и конец двух узлов, найдите путь с максимальной вероятностью успеха от начала до конца и верните его вероятность успеха.
+Если пути от начала до конца нет, верните 0. Ваш ответ будет принят, если он отличается от правильного не более чем на 1e-5. 
+*/
+
+let n = 3;
+let edges = [
+    [0, 1],
+    [1, 2],
+    [0, 2],
+];
+let succProb = [0.5, 0.5, 0.2];
+let start = 0;
+let end = 2;
+
+let maxProbability = function (n, edges, succProb, start, end) {
+    const temp = Array(n).fill(0);
+    const graph = temp.reduce((el, _, i) => el.set(i, []), new Map());
+    edges.forEach(([u, v], i) => {
+        graph.get(u).push([v, succProb[i]]);
+        graph.get(v).push([u, succProb[i]]);
+    });
+
+    const queue = [[start, 1]];
+    temp[start] = 1;
+
+    for (let [node, currPoint] of queue) {
+        for (let [item, nextPoint] of graph.get(node)) {
+            if (currPoint * nextPoint > temp[item]) {
+                temp[item] = currPoint * nextPoint;
+                queue.push([item, temp[item]]);
+            }
+        }
+    }
+
+    return temp[end];
+};
+
+console.log(maxProbability(n, edges, succProb, start, end));
---
+++ task_800/task_750/task_746_m_947.js
@@ -0,0 +1,43 @@
+﻿/**
+На двумерной плоскости мы размещаем n камней в некоторых целочисленных точках координат. В каждой координатной точке может быть не более одного камня.
+Камень можно удалить, если он находится в той же строке или в том же столбце, что и другой камень, который не был удален.
+Учитывая массив камней длины n, где камни[i] = [xi, yi] представляют местоположение i-го камня, верните максимально возможное количество камней, которые можно удалить.
+ */
+
+let stones = [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 2],
+    [2, 1],
+    [2, 2],
+];
+
+let removeStones = function (stones) {
+    const hash = new Set();
+    let cnt = 0;
+
+    function dfs(stone) {
+        const key = stone.join(',');
+        if (hash.has(key)) return;
+        hash.add(key);
+
+        for (let nextStone of stones) {
+            if (nextStone[0] === stone[0] || nextStone[1] === stone[1]) {
+                dfs(nextStone);
+            }
+        }
+    }
+
+    for (let stone of stones) {
+        const key = stone.join(',');
+        if (!hash.has(key)) {
+            cnt++;
+            dfs(stone);
+        }
+    }
+
+    return stones.length - cnt;
+};
+
+console.log(removeStones(stones));
