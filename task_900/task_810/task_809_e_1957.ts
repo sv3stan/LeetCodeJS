ef25abf213e6647a7d1ddec8f76d7260857ad17a
sv3stan <tristan.sergey@gmail.com>
2024-11-02 16:50:13 +0300

    02112024

---
+++ task_900/task_810/task_809_e_1957.ts
@@ -0,0 +1,23 @@
+﻿/**
+Причудливая строка — это строка, в которой нет трех последовательных символов, равных.
+Учитывая строку s, удалите минимально возможное количество символов из s, чтобы сделать ее более интересной.
+Верните последнюю строку после удаления. Можно показать, что ответ всегда будет единственным.
+ */
+
+let s = 'leeetcode';
+
+function makeFancyString(s: string): string {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (
+            stack.length >= 2 &&
+            stack[stack.length - 1] === stack[stack.length - 2] &&
+            stack[stack.length - 1] === s[i]
+        )
+            continue;
+        stack.push(s[i]);
+    }
+    return stack.join('');
+}
+
+console.log(makeFancyString(s));
