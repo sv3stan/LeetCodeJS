e0dc0256cc03d068cc6047e170f061f26a77fce9
sv3stan <tristan.sergey@gmail.com>
2025-01-22 20:38:08 +0300

    22012025

---
+++ task_900/task_890/task_887_h_407.ts
@@ -0,0 +1,56 @@
+﻿/**
+Учитывая целочисленную матрицу m x n heightMap, представляющую высоту каждой элементарной ячейки на двумерной карте высот, верните объем воды, которую она может удержать после дождя.
+ */
+
+function trapRainWater(heightMap: number[][]): number {
+    const rows = heightMap.length;
+    const cols = heightMap[0].length;
+    const minHeap: [number, [number, number]][] = [];
+    const visited: number[][] = Array.from({ length: rows }, () =>
+        Array(cols).fill(0),
+    );
+
+    for (let i = 0; i < cols; i++) {
+        minHeap.push([heightMap[0][i], [0, i]]);
+        minHeap.push([heightMap[rows - 1][i], [rows - 1, i]]);
+        visited[0][i] = 1;
+        visited[rows - 1][i] = 1;
+    }
+
+    for (let i = 1; i < rows - 1; i++) {
+        minHeap.push([heightMap[i][0], [i, 0]]);
+        minHeap.push([heightMap[i][cols - 1], [i, cols - 1]]);
+        visited[i][0] = 1;
+        visited[i][cols - 1] = 1;
+    }
+
+    const delrow = [-1, 0, 1, 0];
+    const delcol = [0, 1, 0, -1];
+    let volume = 0;
+
+    while (minHeap.length > 0) {
+        minHeap.sort((a, b) => a[0] - b[0]);
+        const [height, [curRow, curCol]] = minHeap.shift()!;
+
+        for (let i = 0; i < 4; i++) {
+            const newRow = curRow + delrow[i];
+            const newCol = curCol + delcol[i];
+
+            if (
+                newRow >= 0 &&
+                newCol >= 0 &&
+                newRow < rows &&
+                newCol < cols &&
+                !visited[newRow][newCol]
+            ) {
+                volume += Math.max(height - heightMap[newRow][newCol], 0);
+                minHeap.push([
+                    Math.max(height, heightMap[newRow][newCol]),
+                    [newRow, newCol],
+                ]);
+                visited[newRow][newCol] = 1;
+            }
+        }
+    }
+    return volume;
+}
---
+++ task_900/task_890/task_888_m_2661.ts
@@ -0,0 +1,31 @@
+﻿/**
+Вам дан целочисленный массив arr с нулевым индексом и целочисленная матрица m x n mat. arr и mat содержат все целые числа в диапазоне [1, m * n]. Просмотрите каждый индекс i в arr, начиная с индекса 0, и закрасьте ячейку в mat, содержащую целое число arr[i]. Возвращает наименьший индекс i, при котором строка или столбец будут полностью закрашены матом.
+ */
+
+function firstCompleteIndex(arr: number[], mat: number[][]): number {
+    const m = mat.length;
+    const n = mat[0].length;
+
+    const cellMap: Map<number, [number, number]> = new Map();
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            cellMap.set(mat[i][j], [i, j]);
+        }
+    }
+
+    const rows = Array(m).fill(0);
+    const cols = Array(n).fill(0);
+
+    for (let i = 0; i < arr.length; i++) {
+        const [row, col] = cellMap.get(arr[i])!;
+
+        rows[row]++;
+        cols[col]++;
+
+        if (rows[row] === n || cols[col] === m) {
+            return i;
+        }
+    }
+
+    return -1;
+}
